7de7aaa7bfccae3e5cf2ee5bf5dc39c359504146
Kevin Wong <kevinwong6175@gmail.com>
2017-12-05 04:12:08 +0000

    use Sendgrid and Figaro to mask credentials

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
   if Rails.env.development? || Rails.env.production?
     ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
-        address:        'smtp.gmail.com',
-        port:           587,
+        address:        'smtp.sendgrid.net',
+        port:           2525,
         authentication: 'plain',
-        user_name:      'kevwong8884@gmail.com',
-        password:       'Kw@39768',
-        domain:         'gmail.com',
+        user_name:      ENV['SENDGRID_USERNAME'],
+        password:       ENV['SENDGRID_PASSWORD'],
+        domain:         'heroku.com',
         enable_starttls_auto: true
     }
   end
