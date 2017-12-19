9e992301c6aafe5d44483746ec3355e9252ca195
Kevin Wong <kevinwong6175@gmail.com>
2017-12-19 17:03:12 +0000

    fix heroku deploy

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+  gem 'sqlite3'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
@@ -47,6 +48,7 @@ end
 
 group :production do
    gem 'pg'
+   gem 'rails_12factor'
 end
 
 gem 'devise'
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'devise'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +23,7 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.serve_static_assets = true
   end
 end
