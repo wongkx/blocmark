e1d34b85da08e7083519eb5c24f1c27a6d4f7426
Kevin Wong <kevinwong6175@gmail.com>
2017-12-19 17:07:57 +0000

    fix heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'obscure-caverns-10483.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'blooming-coast-43462.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:        'smtp.sendgrid.net',
